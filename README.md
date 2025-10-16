5e7061d30c92a9871fe745da8ebea4025148b51b
AL-Hareth <alharethturab@gmail.com>
2025-10-16 23:17:52 +0300

    changed the curve and the generator point

--- README.md
+++ README.md
@@ -11,14 +11,14 @@ cargo test
 ## Running the actual private/public key generation
 First go to main and set you parameters
 The variables are:
-`secret` for your secret key
+`secret` for your secret key which is entered as command args
 `gx, gy` for your generator points
 `p` for your modulus
 `a, b` for your elliptic curve coefficients (y^2 = x^3 + ax + b)
 
 running the main:
 ```bash
-cargo run
+cargo run <secret_key_integer>
 ```
 
 Thanks for checking out my app!
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,31 @@ use finite_field::FieldElement;
 use point::Point;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let secret_str: &String = &args[1];
+
     const P: i64 = (1 << 61) - 1;
     const A: i64 = 0;
-    const B: i64 = 1;
+    const B: i64 = 8;
 
     let a = FieldElement::new(A as i128, P as i128);
     let b = FieldElement::new(B as i128, P as i128);
-    let (gx, gy): (i64, i64) = (2, 3);
+    let (gx, gy): (i64, i64) = (2, 4);
     let gfx = FieldElement::new(gx as i128, P as i128);
     let gfy = FieldElement::new(gy as i128, P as i128);
 
     let generator_point = Point::new(Some(gfx), Some(gfy), a, b);
 
-    let secret: i128 = 5001;
+    let secret: i128 = secret_str.parse().unwrap();
 
     let public = generator_point * secret;
 
     println!(
-        "The public key derived from the private key {} is: {}",
+        "Private key: {} \nPublic key: {}",
         secret,
-        public.x.unwrap().value
+        public
+            .x
+            .unwrap_or(FieldElement::new(1 as i128, P as i128))
+            .value
     );
 }
